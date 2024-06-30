689b38d443ddae5751fe53acb5c0d1778f79f355
sanix-darker <s4nixd@gmail.com>
2024-06-30 18:03:27 +0200

    feat: small project to play around

---
+++ project/src/main.rs
@@ -0,0 +1,109 @@
+// common types
+type INT = i32;
+
+fn print_format_name(name: String){
+    println!("Hello '{}'!", name);
+}
+
+fn type_of<T>(_: &T) -> String {
+    let type_name = std::any::type_name::<T>();
+    return format!("{}", type_name);
+}
+
+fn ignore_warning_from_unused_vars(){
+    // 1- by adding the underscore
+    // 2- by adding a 'decorator' for that
+    let _x = 12;
+}
+
+fn time_goes(age: u16) -> u16{
+    let newage = age + 1;
+    return newage;
+}
+
+fn destructured_vars(){
+    let (mut a, b) = (String::from("acid"), 100);
+    a = format!("{}{}", a ,"eee");
+    println!("a={} | b={}", a, b);
+
+    let (x, ..) = (12, 11);
+    let [.., y] = ["a", "b"];
+
+    assert_eq!(x, 12);
+    assert_eq!(y, "b");
+    println!("Hello {} {} !", y, x);
+}
+
+fn separator(rait: INT){
+    for _ in 0..rait{
+        print!("-");
+    }
+    println!("\n");
+}
+
+fn multiple_type_in_array(){
+
+    // let x:u16 = 12_u8 as u16;
+
+    let mut arr: Vec<Box<dyn std::any::Any>> = Vec::new();
+
+    arr.push(Box::new(42));
+    arr.push(Box::new("hello"));
+
+    separator(12);
+
+    for item in &arr {
+        if let Some(i) = item.downcast_ref::<i32>() {
+            println!("i32: {}", i);
+        } else if let Some(s) = item.downcast_ref::<&str>() {
+            println!("&str: {}", s);
+        }
+    }
+}
+
+fn main() {
+    //  Variable that is going to be used
+    //  should be initialised with a value
+    let name: &str = "sanix";
+    // For assertions :
+    // assert_eq!(name, "dk");
+    print_format_name(
+        name.to_string()
+    );
+
+    let mut vector = vec![];
+    vector.push("a");
+
+    let hobbies: [&str; 2] = ["draw", "code"];
+    println!("as hobbies: {:?}", hobbies);
+    // we can also shadowing variable by redeclared them
+    // We can shadowing with a value from another data type.
+    //
+    // let hobbies: [&str; 1] = ["draw"];
+    // println!("as hobbies: {:?}", hobbies);
+
+    // custom isolated scope.
+    {
+        // A variable that will be changed, need
+        // to be set with the 'mut' attr.
+        let mut age: u16 = 30;
+        // note ux and iy for unsigned and signed
+        age = time_goes(age);
+
+        println!("You have {} years old.", age);
+    }
+
+    // How to ignore unsuded vars:
+    ignore_warning_from_unused_vars();
+
+    // how to destructure vars from tuple
+    destructured_vars();
+
+    // a multiple type in array
+    multiple_type_in_array();
+
+    // to get the type of a value
+    println!("> type of hobbies is '{}'", type_of(&hobbies));
+
+    // loop on a -> Z
+}
